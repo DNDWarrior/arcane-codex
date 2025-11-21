ad7f11b0b90cef4b300320d49035ba40cfca23cf
DNDWarrior <lukemaccadies@gmail.com>
2025-11-21 11:52:51 -0800

    Add files via upload

---
+++ items.js
@@ -0,0 +1,20 @@
+// ARCANE CODEX - ITEM DATABASE
+// Simple list of strings for now
+
+window.DB_ITEMS = [
+    "Rations (1 week)", 
+    "Torch", 
+    "Bedroll", 
+    "Waterskin", 
+    "Healer's Tools", 
+    "Thieves' Tools", 
+    "Writing Kit", 
+    "Healing Potion (Minor)", 
+    "Healing Potion (Lesser)",
+    "Material: Diamond Dust (25gp)", 
+    "Material: Onyx Gems (50gp)", 
+    "Material: Bat Guano",
+    "Material: Black Pearl (500 gp)",
+    "Scroll of Magic Missile", 
+    "Wand of Heal"
+];
